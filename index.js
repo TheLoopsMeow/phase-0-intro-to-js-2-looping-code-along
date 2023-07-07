4e6e667c44c846afa2af325bed22e9d85e7caf25
Wyatt <wyattstephenson@rocketmail.com>
2023-07-07 13:46:02 -0600

    Intro to JS 2 Looping completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards (array, evenement) {
+  const anotherArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+      anotherArray[i] = `Thank you, ${array[i]}, for the wonderful ${evenement} gift!`
+  }
+return anotherArray;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown (integer) {
+  while (integer >= 0) {
+    console.log(integer);
+    integer--;
+  }
+}
+
+countDown(10);
